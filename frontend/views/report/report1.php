765b480b376d45fc6a835a53a7ae47b82f00dc5f
DHDC Main <utehnphnu2015@gmail.com>
2015-03-14 17:21:37 +0700

    แก้ อะรไก็ว่าไป

--- frontend/views/report/report1.php
+++ frontend/views/report/report1.php
@@ -1,6 +1,9 @@
 <?php
 use yii\grid\GridView;
 
+$this->params['breadcrumbs'][] = ['label' => 'หน้ารายงานหลัก', 'url' => ['report/index']];
+$this->params['breadcrumbs'][] = 'รายงานตัวชี้วัดที่ 1';
+
 echo GridView::widget([
     'dataProvider'=>$dataProvider
 ]);

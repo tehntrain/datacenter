67f46a34703bc7bbffb5916281dba3f977dc7a8e
DHDC Main <utehnphnu2015@gmail.com>
2015-03-14 17:23:20 +0700

    แก้ อะรไก็ว่าไป

--- frontend/views/report/report1.php
+++ frontend/views/report/report1.php
@@ -3,7 +3,12 @@ use yii\grid\GridView;
 
 $this->params['breadcrumbs'][] = ['label' => 'หน้ารายงานหลัก', 'url' => ['report/index']];
 $this->params['breadcrumbs'][] = 'รายงานตัวชี้วัดที่ 1';
+?>
 
+<div class="well">
+    รายงานตัวชี้วัดที่ 1
+</div>
+<?php
 echo GridView::widget([
     'dataProvider'=>$dataProvider
 ]);

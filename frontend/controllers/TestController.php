18f60cd8758f17e774fa9a93d436e463b472a8f9
DHDC Main <utehnphnu2015@gmail.com>
2015-03-14 14:08:40 +0700

    แก้ อะรไก็ว่าไป

--- frontend/controllers/TestController.php
+++ frontend/controllers/TestController.php
@@ -12,27 +12,39 @@ class TestController extends \yii\web\Controller {
 
         echo "Test1";
         echo "<hr>";
-        $data1 = array(0,2,1,3);
-        $data2= [0,2,1,3];
-        
-        $data3 = ['1'=>'a','2'=>'b','4'=>'0'];
-        
+        $data1 = array(0, 2, 1, 3);
+        $data2 = [0, 2, 1, 3];
+
+        $data3 = ['1' => 'a', '2' => 'b', '4' => '0'];
+
         print_r($data1);
         echo "<hr>";
         print_r($data2);
         echo "<hr>";
         print_r($data3);
-        
     }
 
 // จบ test1
 
     public function actionTest2() {
-
-        echo "Test2";
+        
+        $cid = '3651000030203';
+        
+        return $this->render('test2', [
+                    'name' => 'อุเทน',
+                    'lname' => 'จาดยางโทน',
+                    'sex' => 'ชาย',
+                    'bdate'=>date('Y-m-d'),
+                    'cid'=>$cid
+        ]);
+    }
+    
+    public function actionTest3($name){
+        return $this->render('test3',[
+            'name'=>$name
+        ]);
     }
 
-// จบ test1
 }
 
 //จบ class
---
+++ frontend/views/test/test2.php
@@ -0,0 +1,15 @@
+<?php
+
+echo date('Y-m-d');
+echo "<hr>";
+echo "ค่าที่ส่งมาจาก action คือ";
+echo $name;
+echo $lname;
+echo $sex;
+
+echo "<hr>";
+echo $cid;
+echo "<hr>";
+echo $bdate;
+
+

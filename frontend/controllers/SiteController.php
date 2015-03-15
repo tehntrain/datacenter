20bbb999a9a6dae9193322f9de97f43c93d45c79
DHDC Main <utehnphnu2015@gmail.com>
2015-03-15 10:02:08 +0700

    แก้ อะรไก็ว่าไป

--- frontend/controllers/SiteController.php
+++ frontend/controllers/SiteController.php
@@ -121,9 +121,7 @@ class SiteController extends Controller
         $model = new SignupForm();
         if ($model->load(Yii::$app->request->post())) {
             if ($user = $model->signup()) {
-                if (Yii::$app->getUser()->login($user)) {
-                    return $this->goHome();
-                }
+                return $this->redirect(['login']);
             }
         }
 
